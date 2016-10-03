300d48bebd95cd49e4b42b53da11450c518e0b8b
thiloyes <thilo_n@gmx.de>
2016-10-03 12:40:30 +0200

    Remove dependency on django-timedelta-field
    Use the parse function from utils.py instead.

--- drip/models.py
+++ drip/models.py
@@ -6,11 +6,8 @@ from django.core.exceptions import ValidationError
 from django.conf import settings
 from django.utils.functional import cached_property
 
-from drip.utils import get_user_model
+from drip.utils import get_user_model, parse
 
-# just using this to parse, but totally insane package naming...
-# https://bitbucket.org/schinckel/django-timedelta-field/
-import timedelta as djangotimedelta
 
 class Drip(models.Model):
     date = models.DateTimeField(auto_now_add=True)
@@ -139,16 +136,16 @@ class QuerySetRule(models.Model):
         # set time deltas and dates
         if self.field_value.startswith('now-'):
             field_value = self.field_value.replace('now-', '')
-            field_value = now() - djangotimedelta.parse(field_value)
+            field_value = now() - parse(field_value)
         elif self.field_value.startswith('now+'):
             field_value = self.field_value.replace('now+', '')
-            field_value = now() + djangotimedelta.parse(field_value)
+            field_value = now() + parse(field_value)
         elif self.field_value.startswith('today-'):
             field_value = self.field_value.replace('today-', '')
-            field_value = now().date() - djangotimedelta.parse(field_value)
+            field_value = now().date() - parse(field_value)
         elif self.field_value.startswith('today+'):
             field_value = self.field_value.replace('today+', '')
-            field_value = now().date() + djangotimedelta.parse(field_value)
+            field_value = now().date() + parse(field_value)
 
         # F expressions
         if self.field_value.startswith('F_'):
